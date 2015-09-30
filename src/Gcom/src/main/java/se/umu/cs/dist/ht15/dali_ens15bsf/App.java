c5562b04eb3417273fefeadce4aca6499571436e
dali <dali@itchy.cs.umu.se>
2015-09-30 22:41:05 +0200

    Initialized empty main

---
+++ src/Gcom/src/main/java/se/umu/cs/dist/ht15/dali_ens15bsf/App.java
@@ -0,0 +1,13 @@
+package se.umu.cs.dist.ht15.dali_ens15bsf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
