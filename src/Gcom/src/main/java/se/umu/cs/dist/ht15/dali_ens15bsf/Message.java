86ec414a022537761ad906d99d10ebfae67c5cdc
dali_umu <dali@cs.umu.se>
2015-10-03 13:39:46 +0000

    Initiated Message class, feels unnecessary to have it as an interface at this point

---
+++ src/Gcom/src/main/java/se/umu/cs/dist/ht15/dali_ens15bsf/Message.java
@@ -0,0 +1,18 @@
+package se.umu.cs.dist.ht15.dali_ens15bsf;
+
+import se.umu.cs.dist.ht15.dali_ens15bsf.time.VectorClock;
+
+public class Message {
+
+	public Integer getID() {
+		return null;
+	}
+
+	public String getContent() {
+		return null;
+	}
+
+	public VectorClock getClock() {
+		return null;
+	}
+}
