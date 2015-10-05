a9f395e2b1fad7a67981d9f07dbf316882ae36e5
blasterbug <blasterbug@users.noreply.github.com>
2015-10-05 16:56:43 +0200

    RMI Service debut

--- src/Gcom/src/main/java/se/umu/cs/dist/ht15/dali_ens15bsf/com/CommMessage.java
+++ src/Gcom/src/main/java/se/umu/cs/dist/ht15/dali_ens15bsf/com/CommMessage.java
@@ -1,12 +1,14 @@
 package Gcom.src.main.java.se.umu.cs.dist.ht15.dali_ens15bsf.com;
 
+import java.io.Serializable;
+
 /**
  * Created by benjamin on 04/10/15.
  *
  * CommunicationService Message
  *
  */
-public class CommMessage
+public class CommMessage implements Serializable
 {
     public CommMessage()
     {
---
+++ src/Gcom/src/main/java/se/umu/cs/dist/ht15/dali_ens15bsf/com/RMIService.java
@@ -0,0 +1,38 @@
+package Gcom.src.main.java.se.umu.cs.dist.ht15.dali_ens15bsf.com;
+
+import java.io.Serializable;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.server.RMIClientSocketFactory;
+import java.rmi.server.RMIServerSocketFactory;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ * Created by ens15bsf on 2015-10-05.
+ */
+public class RMIService extends UnicastRemoteObject implements Remote, Serializable
+{
+  /**
+   * @see java.io.Serializable
+   */
+  protected RMIService ( int port, RMIClientSocketFactory csf, RMIServerSocketFactory ssf ) throws RemoteException
+  {
+    super( port, csf, ssf );
+  }
+
+  /**
+   * @see java.io.Serializable
+   */
+  protected RMIService () throws RemoteException
+  {
+  }
+
+  /**
+   * @see java.io.Serializable
+   */
+  protected RMIService ( int port ) throws RemoteException
+  {
+    super( port );
+  }
+
+}
