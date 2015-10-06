64014121cb0d534b90c2a808644e55824feee9df
blasterbug <blasterbug@users.noreply.github.com>
2015-10-06 11:02:58 +0200

    start commService  implementation

--- src/Gcom/src/main/java/se/umu/cs/dist/ht15/dali_ens15bsf/com/RMIService.java
+++ src/Gcom/src/main/java/se/umu/cs/dist/ht15/dali_ens15bsf/com/RMIService.java
@@ -1,6 +1,7 @@
 package Gcom.src.main.java.se.umu.cs.dist.ht15.dali_ens15bsf.com;
 
 import java.io.Serializable;
+import java.rmi.Naming;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.server.RMIClientSocketFactory;
@@ -10,29 +11,10 @@ import java.rmi.server.UnicastRemoteObject;
 /**
  * Created by ens15bsf on 2015-10-05.
  */
-public class RMIService extends UnicastRemoteObject implements Remote, Serializable
+public class RMIService
 {
-  /**
-   * @see java.rmi.server.UnicastRemoteObject
-   */
-  protected RMIService ( int port, RMIClientSocketFactory csf, RMIServerSocketFactory ssf ) throws RemoteException
-  {
-    super( port, csf, ssf );
-  }
+  protected Node node;
 
-  /**
-   * @see java.rmi.server.UnicastRemoteObject
-   */
-  protected RMIService () throws RemoteException
-  {
-  }
-
-  /**
-   * @see java.rmi.server.UnicastRemoteObject
-   */
-  protected RMIService ( int port ) throws RemoteException
-  {
-    super( port );
-  }
+  public RMIService
 
 }
