a0f967d322fba4769360994ec7d2744fbddd483e
dali <dali@itchy.cs.umu.se>
2015-10-07 17:15:47 +0200

    Tests for member and nameserver

---
+++ src/Gcom/src/test/java/se/umu/cs/dist/ht15/dali_ens15bsf/MemberTest.java
@@ -0,0 +1,17 @@
+package se.umu.cs.dist.ht15.dali_ens15bsf;
+
+import org.junit.Test;
+import junit.framework.Assert;
+
+import se.umu.cs.dist.ht15.dali_ens15bsf.groupmanagement.Member;
+import se.umu.cs.dist.ht15.dali_ens15bsf.groupmanagement.MemberImpl;
+
+import se.umu.cs.dist.ht15.dali_ens15bsf.nameserver.NameServer;
+import se.umu.cs.dist.ht15.dali_ens15bsf.nameserver.NameServerImpl;
+
+
+
+
+public class MemberTest {
+
+}
---
+++ src/Gcom/src/test/java/se/umu/cs/dist/ht15/dali_ens15bsf/NameserverTest.java
@@ -0,0 +1,73 @@
+package se.umu.cs.dist.ht15.dali_ens15bsf;
+
+import org.junit.Test;
+import junit.framework.Assert;
+
+import se.umu.cs.dist.ht15.dali_ens15bsf.groupmanagement.Member;
+import se.umu.cs.dist.ht15.dali_ens15bsf.groupmanagement.MemberImpl;
+
+import se.umu.cs.dist.ht15.dali_ens15bsf.nameserver.NameServer;
+import se.umu.cs.dist.ht15.dali_ens15bsf.nameserver.NameServerImpl;
+
+
+
+
+public class NameserverTest {
+
+	@Test
+	public void shouldNotReturnNullOnNoGroups() {
+		NameServer server = new NameServerImpl();
+
+		Assert.assertTrue(server.getGroups() != null);
+	}
+
+	@Test
+	public void shouldCreateGroupOnNonexistant() {
+		NameServer server = new NameServerImpl();
+
+		Member m = new MemberImpl();
+		
+		server.joinGroup("g1", m);
+
+		Assert.assertTrue(!server.getGroups().isEmpty());
+		
+	}
+
+
+	@Test
+	public void shouldReturnMemberOnNonexistant() {
+		NameServer server = new NameServerImpl();
+
+		Member m1 = new MemberImpl();
+
+		Member m2 = server.joinGroup("g1", m1);
+
+		Assert.assertTrue(m1.equals(m2));
+	}
+
+	@Test
+	public void shouldCreateCorrectGroupname() {
+		NameServer server = new NameServerImpl();
+
+		Member m = new MemberImpl();
+
+		server.joinGroup("g1", m);
+
+		Assert.assertTrue(server.getGroups().contains("g1"));
+	}
+
+	@Test
+	public void shouldReturnPreviousLeaderOnSecondJoin() {
+		NameServer server = new NameServerImpl();
+
+		Member m1 = new MemberImpl();
+		Member m2 = new MemberImpl();
+
+		server.joinGroup("g1", m1);
+		Member m3 = server.joinGroup("g1", m2);
+
+		Assert.assertTrue(m3.equals(m1));
+	}
+
+}
+
