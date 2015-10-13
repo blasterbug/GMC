67fa5a3b2f9aa22c190222b8b915d34e8dd77530
blasterbug <blasterbug@users.noreply.github.com>
2015-10-13 12:24:39 +0200

    Starting a debug thing

---
+++ src/Gcom/src/main/java/se/umu/cs/dist/ht15/dali_ens15bsf/com/debug/CommDebug.java
@@ -0,0 +1,59 @@
+package se.umu.cs.dist.ht15.dali_ens15bsf.com.debug;
+
+import se.umu.cs.dist.ht15.dali_ens15bsf.com.CommMember;
+import se.umu.cs.dist.ht15.dali_ens15bsf.com.CommMessage;
+import se.umu.cs.dist.ht15.dali_ens15bsf.com.MulticastStrategy;
+import se.umu.cs.dist.ht15.dali_ens15bsf.com.RemoteMember;
+
+import java.rmi.RemoteException;
+import java.util.Collection;
+import java.util.LinkedList;
+
+/**
+ * Created by ens15bsf on 2015-10-13.
+ */
+public class CommDebug extends CommMember
+{
+  private LinkedList<CommMessage> delayedMessages;
+  private long messageDelay;
+  /**
+   * Create a new node
+   *
+   * @param strategy Strategy to use for multicasting messages
+   */
+  public CommDebug ( MulticastStrategy strategy )
+  {
+    super( strategy );
+    delayedMessages = new LinkedList<CommMessage>();
+    messageDelay = 0;
+  }
+
+  /**
+   * Set the amont of time to wait before sending a message
+   * @param delay delay to wait before sending message, in millis
+   */
+  public void setMessageDelay( long delay )
+  {
+    messageDelay = delay;
+  }
+
+
+  /**
+   * Send a message to the group
+   * @param msg   Message to multicast
+   * @param group Group to send the message
+   * @throws RemoteException
+   */
+  @Override
+  public void post ( CommMessage msg, Collection<RemoteMember> group ) throws RemoteException
+  {
+    try
+    {
+      Thread.sleep( messageDelay );
+    } catch ( InterruptedException e )
+    {
+      e.printStackTrace();
+    }
+    commStrategy.send( msg, group );
+  }
+}
